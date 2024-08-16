6618c61683781f3afbecbaded621b38e06f4c8ce
Phalanndwa Tendani <2023393305@ufs4life.ac.za>
2024-08-16 12:38:00 +0200

    Add files via upload
    The app is yet to be fully polished but it is functional,enjoy

---
+++ Accomodation/Form1.Designer.cs
@@ -0,0 +1,389 @@
+﻿namespace _2023393305_Question2._1
+{
+    partial class frmAccomodationSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSurname = new System.Windows.Forms.Label();
+            this.lblInitials = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblMoveInDate = new System.Windows.Forms.Label();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblFormColors = new System.Windows.Forms.Label();
+            this.rbtnKhaki = new System.Windows.Forms.RadioButton();
+            this.rbtnLime = new System.Windows.Forms.RadioButton();
+            this.rbtnYellow = new System.Windows.Forms.RadioButton();
+            this.rbtnSilver = new System.Windows.Forms.RadioButton();
+            this.rbtnCoral = new System.Windows.Forms.RadioButton();
+            this.rbtnGold = new System.Windows.Forms.RadioButton();
+            this.lblStudentNumber = new System.Windows.Forms.Label();
+            this.txtStudentNumber = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.txtInitials = new System.Windows.Forms.TextBox();
+            this.lblUserDetails = new System.Windows.Forms.Label();
+            this.nudAge = new System.Windows.Forms.NumericUpDown();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.dtpMoveInDate = new System.Windows.Forms.DateTimePicker();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSurname
+            // 
+            this.lblSurname.AutoSize = true;
+            this.lblSurname.Location = new System.Drawing.Point(42, 129);
+            this.lblSurname.Name = "lblSurname";
+            this.lblSurname.Size = new System.Drawing.Size(49, 13);
+            this.lblSurname.TabIndex = 2;
+            this.lblSurname.Text = "Surname";
+            // 
+            // lblInitials
+            // 
+            this.lblInitials.AutoSize = true;
+            this.lblInitials.Location = new System.Drawing.Point(55, 175);
+            this.lblInitials.Name = "lblInitials";
+            this.lblInitials.Size = new System.Drawing.Size(36, 13);
+            this.lblInitials.TabIndex = 3;
+            this.lblInitials.Text = "Initials";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(49, 220);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(42, 13);
+            this.lblGender.TabIndex = 4;
+            this.lblGender.Text = "Gender";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(65, 265);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(26, 13);
+            this.lblAge.TabIndex = 5;
+            this.lblAge.Text = "Age";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(53, 308);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(38, 13);
+            this.lblPhone.TabIndex = 6;
+            this.lblPhone.Text = "Phone";
+            // 
+            // lblMoveInDate
+            // 
+            this.lblMoveInDate.AutoSize = true;
+            this.lblMoveInDate.Location = new System.Drawing.Point(19, 347);
+            this.lblMoveInDate.Name = "lblMoveInDate";
+            this.lblMoveInDate.Size = new System.Drawing.Size(72, 13);
+            this.lblMoveInDate.TabIndex = 7;
+            this.lblMoveInDate.Text = "Move In Date";
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(16, 412);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(75, 23);
+            this.btnApply.TabIndex = 9;
+            this.btnApply.Text = "Apply";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(370, 412);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(555, 415);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 11;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblFormColors
+            // 
+            this.lblFormColors.AutoSize = true;
+            this.lblFormColors.Location = new System.Drawing.Point(56, 0);
+            this.lblFormColors.Name = "lblFormColors";
+            this.lblFormColors.Size = new System.Drawing.Size(62, 13);
+            this.lblFormColors.TabIndex = 0;
+            this.lblFormColors.Text = "Form Colors";
+            // 
+            // rbtnKhaki
+            // 
+            this.rbtnKhaki.AutoSize = true;
+            this.rbtnKhaki.Location = new System.Drawing.Point(59, 35);
+            this.rbtnKhaki.Name = "rbtnKhaki";
+            this.rbtnKhaki.Size = new System.Drawing.Size(52, 17);
+            this.rbtnKhaki.TabIndex = 7;
+            this.rbtnKhaki.TabStop = true;
+            this.rbtnKhaki.Text = "Khaki";
+            this.rbtnKhaki.UseVisualStyleBackColor = true;
+            this.rbtnKhaki.CheckedChanged += new System.EventHandler(this.rbtnKhaki_CheckedChanged);
+            this.rbtnKhaki.Click += new System.EventHandler(this.radioButton_Click);
+            // 
+            // rbtnLime
+            // 
+            this.rbtnLime.AutoSize = true;
+            this.rbtnLime.Location = new System.Drawing.Point(59, 86);
+            this.rbtnLime.Name = "rbtnLime";
+            this.rbtnLime.Size = new System.Drawing.Size(47, 17);
+            this.rbtnLime.TabIndex = 8;
+            this.rbtnLime.TabStop = true;
+            this.rbtnLime.Text = "Lime";
+            this.rbtnLime.UseVisualStyleBackColor = true;
+            this.rbtnLime.Click += new System.EventHandler(this.radioButton_Click);
+            // 
+            // rbtnYellow
+            // 
+            this.rbtnYellow.AutoSize = true;
+            this.rbtnYellow.Location = new System.Drawing.Point(59, 132);
+            this.rbtnYellow.Name = "rbtnYellow";
+            this.rbtnYellow.Size = new System.Drawing.Size(56, 17);
+            this.rbtnYellow.TabIndex = 9;
+            this.rbtnYellow.TabStop = true;
+            this.rbtnYellow.Text = "Yellow";
+            this.rbtnYellow.UseVisualStyleBackColor = true;
+            this.rbtnYellow.Click += new System.EventHandler(this.radioButton_Click);
+            // 
+            // rbtnSilver
+            // 
+            this.rbtnSilver.AutoSize = true;
+            this.rbtnSilver.Location = new System.Drawing.Point(59, 188);
+            this.rbtnSilver.Name = "rbtnSilver";
+            this.rbtnSilver.Size = new System.Drawing.Size(51, 17);
+            this.rbtnSilver.TabIndex = 10;
+            this.rbtnSilver.TabStop = true;
+            this.rbtnSilver.Text = "Silver";
+            this.rbtnSilver.UseVisualStyleBackColor = true;
+            this.rbtnSilver.Click += new System.EventHandler(this.radioButton_Click);
+            // 
+            // rbtnCoral
+            // 
+            this.rbtnCoral.AutoSize = true;
+            this.rbtnCoral.Location = new System.Drawing.Point(59, 248);
+            this.rbtnCoral.Name = "rbtnCoral";
+            this.rbtnCoral.Size = new System.Drawing.Size(49, 17);
+            this.rbtnCoral.TabIndex = 11;
+            this.rbtnCoral.TabStop = true;
+            this.rbtnCoral.Text = "Coral";
+            this.rbtnCoral.UseVisualStyleBackColor = true;
+            this.rbtnCoral.Click += new System.EventHandler(this.radioButton_Click);
+            // 
+            // rbtnGold
+            // 
+            this.rbtnGold.AutoSize = true;
+            this.rbtnGold.Location = new System.Drawing.Point(59, 300);
+            this.rbtnGold.Name = "rbtnGold";
+            this.rbtnGold.Size = new System.Drawing.Size(47, 17);
+            this.rbtnGold.TabIndex = 12;
+            this.rbtnGold.TabStop = true;
+            this.rbtnGold.Text = "Gold";
+            this.rbtnGold.UseVisualStyleBackColor = true;
+            this.rbtnGold.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            this.rbtnGold.Click += new System.EventHandler(this.radioButton_Click);
+            // 
+            // lblStudentNumber
+            // 
+            this.lblStudentNumber.AutoSize = true;
+            this.lblStudentNumber.Location = new System.Drawing.Point(3, 35);
+            this.lblStudentNumber.Name = "lblStudentNumber";
+            this.lblStudentNumber.Size = new System.Drawing.Size(84, 13);
+            this.lblStudentNumber.TabIndex = 1;
+            this.lblStudentNumber.Text = "Student Number";
+            // 
+            // txtStudentNumber
+            // 
+            this.txtStudentNumber.Location = new System.Drawing.Point(93, 32);
+            this.txtStudentNumber.Name = "txtStudentNumber";
+            this.txtStudentNumber.Size = new System.Drawing.Size(181, 20);
+            this.txtStudentNumber.TabIndex = 15;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.Location = new System.Drawing.Point(93, 79);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(181, 20);
+            this.txtSurname.TabIndex = 14;
+            // 
+            // txtInitials
+            // 
+            this.txtInitials.Location = new System.Drawing.Point(93, 125);
+            this.txtInitials.Name = "txtInitials";
+            this.txtInitials.Size = new System.Drawing.Size(181, 20);
+            this.txtInitials.TabIndex = 13;
+            // 
+            // lblUserDetails
+            // 
+            this.lblUserDetails.AutoSize = true;
+            this.lblUserDetails.Location = new System.Drawing.Point(27, 27);
+            this.lblUserDetails.Name = "lblUserDetails";
+            this.lblUserDetails.Size = new System.Drawing.Size(64, 13);
+            this.lblUserDetails.TabIndex = 0;
+            this.lblUserDetails.Text = "User Details";
+            // 
+            // nudAge
+            // 
+            this.nudAge.Location = new System.Drawing.Point(93, 215);
+            this.nudAge.Name = "nudAge";
+            this.nudAge.Size = new System.Drawing.Size(181, 20);
+            this.nudAge.TabIndex = 16;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(93, 258);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(181, 20);
+            this.txtPhone.TabIndex = 12;
+            // 
+            // dtpMoveInDate
+            // 
+            this.dtpMoveInDate.Location = new System.Drawing.Point(93, 296);
+            this.dtpMoveInDate.Name = "dtpMoveInDate";
+            this.dtpMoveInDate.Size = new System.Drawing.Size(181, 20);
+            this.dtpMoveInDate.TabIndex = 17;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(10, 43);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.cmbGender);
+            this.splitContainer1.Panel1.Controls.Add(this.dtpMoveInDate);
+            this.splitContainer1.Panel1.Controls.Add(this.txtPhone);
+            this.splitContainer1.Panel1.Controls.Add(this.nudAge);
+            this.splitContainer1.Panel1.Controls.Add(this.txtInitials);
+            this.splitContainer1.Panel1.Controls.Add(this.txtSurname);
+            this.splitContainer1.Panel1.Controls.Add(this.txtStudentNumber);
+            this.splitContainer1.Panel1.Controls.Add(this.lblStudentNumber);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.rbtnGold);
+            this.splitContainer1.Panel2.Controls.Add(this.rbtnCoral);
+            this.splitContainer1.Panel2.Controls.Add(this.rbtnSilver);
+            this.splitContainer1.Panel2.Controls.Add(this.rbtnYellow);
+            this.splitContainer1.Panel2.Controls.Add(this.rbtnLime);
+            this.splitContainer1.Panel2.Controls.Add(this.rbtnKhaki);
+            this.splitContainer1.Panel2.Controls.Add(this.lblFormColors);
+            this.splitContainer1.Size = new System.Drawing.Size(620, 363);
+            this.splitContainer1.SplitterDistance = 433;
+            this.splitContainer1.TabIndex = 17;
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Location = new System.Drawing.Point(93, 169);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(181, 21);
+            this.cmbGender.TabIndex = 18;
+            this.cmbGender.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // frmAccomodationSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(714, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.lblMoveInDate);
+            this.Controls.Add(this.lblUserDetails);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblInitials);
+            this.Controls.Add(this.lblSurname);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "frmAccomodationSystem";
+            this.Text = "Accomodation System";
+            this.Load += new System.EventHandler(this.frmAccomodationSystem_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAge)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblSurname;
+        private System.Windows.Forms.Label lblInitials;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblMoveInDate;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblFormColors;
+        private System.Windows.Forms.RadioButton rbtnKhaki;
+        private System.Windows.Forms.RadioButton rbtnLime;
+        private System.Windows.Forms.RadioButton rbtnYellow;
+        private System.Windows.Forms.RadioButton rbtnSilver;
+        private System.Windows.Forms.RadioButton rbtnCoral;
+        private System.Windows.Forms.RadioButton rbtnGold;
+        private System.Windows.Forms.Label lblStudentNumber;
+        private System.Windows.Forms.TextBox txtStudentNumber;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.TextBox txtInitials;
+        private System.Windows.Forms.Label lblUserDetails;
+        private System.Windows.Forms.NumericUpDown nudAge;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.DateTimePicker dtpMoveInDate;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ComboBox cmbGender;
+    }
+}
+
---
+++ Accomodation/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2023393305_Question2._1
+{
+    public partial class frmAccomodationSystem : Form
+    {
+        public frmAccomodationSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbtnKhaki_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void radioButton_Click(object sender, EventArgs e)
+        {
+            RadioButton rbtnClicked = (RadioButton)sender;
+
+            if (rbtnClicked == rbtnKhaki)
+            {
+                this.BackColor = Color.Khaki;
+            }
+            else if (rbtnClicked == rbtnLime )
+            {
+                this.BackColor = Color.Lime;
+            }
+            else if (rbtnClicked ==rbtnYellow)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            else if (rbtnClicked == rbtnSilver)
+            {
+                this.BackColor = Color.Silver;
+            }
+            else if (rbtnClicked == rbtnCoral)
+            {
+                this.BackColor = Color.Coral;
+            }
+            else if (rbtnClicked==rbtnGold)
+            {
+                this.BackColor = Color.Gold;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInitials.Clear();
+            txtPhone.Clear();
+            txtStudentNumber.Clear();
+            txtSurname.Clear();
+            nudAge.ResetText();
+            dtpMoveInDate.ResetText();
+            cmbGender.ResetText();
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            string sStudentNo = txtStudentNumber.Text;
+            string sSurname = txtSurname.Text;
+            string sInitials = txtInitials.Text;
+            string sPhone = txtPhone.Text;
+            string sGender = cmbGender.Text;
+            string sAge = nudAge.Text;
+            string sMoveInDate = dtpMoveInDate.Value.ToShortDateString();
+
+            if (string.IsNullOrWhiteSpace(txtStudentNumber.Text)) 
+            {
+                MessageBox.Show("Student Number is missing","Confirmation",MessageBoxButtons.RetryCancel,MessageBoxIcon.Error);
+            }
+            else if (string.IsNullOrWhiteSpace(txtSurname.Text))
+            {
+                MessageBox.Show("Surname is missing", "Confirmation", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            else if (string.IsNullOrWhiteSpace(txtInitials.Text))
+            {
+                MessageBox.Show("Initials are missing", "Confirmation", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            else if (cmbGender.SelectedIndex==-1)
+            {
+                MessageBox.Show("Select a valid gender", "Confirmation", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            else if (!int.TryParse(nudAge.Text,out int age) || age <=0)
+            {
+                MessageBox.Show("Please select a valid age ", "Confirmation", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            else if (string.IsNullOrWhiteSpace(txtPhone.Text))
+            {
+                MessageBox.Show("Phone Number is missing", "Confirmation", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Thank you for your application.The following are your details: \n" + "\n"
+                  + "Student Number: " + sStudentNo + "\n" + "Surname: " + sSurname + "\n"
+                  + "Initials: " + sInitials + "\n" + "Gender: "+sGender + "\n" + "Age: " + sAge + "\n" + "Phone: " + sPhone +"\n" +
+                  "Date: " + sMoveInDate, "Confirmation", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void frmAccomodationSystem_Load(object sender, EventArgs e)
+        {
+            cmbGender.Items.Add("Male");
+            cmbGender.Items.Add("Female");
+        }
+    }
+}
